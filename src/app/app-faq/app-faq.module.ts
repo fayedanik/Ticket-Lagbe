0df43f6bfda9ac9dc71d094ea22f899c28bc5670
fayedanik <fayed.anik@selise.ch>
2021-08-25 23:29:42 +0600

    committed

--- src/app/app-faq/app-faq.module.ts
+++ src/app/app-faq/app-faq.module.ts
@@ -2,12 +2,16 @@
 import { NgModule  } from "@angular/core";
 import { AppFaqComponent } from "./components/app-faq.component";
 import { AppFaqRoutingModule } from "./app-faq.routing";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatExpansionModule } from '@angular/material/expansion';
 @NgModule({
     declarations:[
         AppFaqComponent
     ],
     imports:[
-        AppFaqRoutingModule
+        AppFaqRoutingModule,
+        FlexLayoutModule,
+        MatExpansionModule
     ],
     exports:[]
 })
